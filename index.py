db0f090f1362ae0da7aa2d1cc653c9fdf937cdb7
msilberg <michaels@uprisenow.com>
2016-07-20 13:21:19 +0300

    v0.2

--- index.py
+++ index.py
@@ -1,2 +1,73 @@
-foo = "hello world"
-print (foo)
+# fooTuples = (1, 2, 3, 4, 5)
+# print type(fooTuples)
+# print (fooTuples)
+#
+# fooDictionary = {'one': 1, 'two': 2, 'three': 3}
+# print type(fooDictionary)
+# print (fooDictionary)
+
+# --== Data Type Conversions ==--
+# foo = '1'
+# print type(foo)
+# print (foo)
+# foo2 = float(foo)
+# print type(foo2)
+# print (foo2)
+#
+# fooDictionary = {'one': 1, 'two': 2, 'three': 3}
+# print type(fooDictionary)
+# fooDictionary2 = repr(fooDictionary)
+# print (fooDictionary2)
+# print type(fooDictionary2)
+
+# foo = 1
+# print eval("foo + 2")
+
+# --=== ++++ ===--
+# fooStr = "spacebuttonisbrokenonmykeyboardsowhatcanido"
+
+# tupleVar = tuple(fooStr)
+# print (tupleVar)
+
+# listVar = list(fooStr)
+# print (listVar)
+
+# setVar = set(fooStr)
+# print setVar
+
+# ** Foreach **
+# d1 = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five'}
+# d2 = {}
+# for k, v in d1.items():
+#     d2[k] = v
+# ** Dict Way **
+# d2 = dict((k, v) for k, v in d1.items())
+# print type(d2)
+# print (d2)
+
+# fooFrozen = frozenset(fooStr)
+# print type(fooFrozen)
+# print (fooFrozen)
+
+# ** integer to char **
+# foo = 1259
+# foo2 = chr(foo)
+# foo2 = unichr(foo)
+# foo2 = ord(foo)
+# print type(foo2)
+# print (foo2)
+
+# foo = 10
+# foo2 = hex(foo)
+# foo2 = oct(foo)
+# print type(foo2)
+# print (foo2)
+
+# ** Operators **
+# a = 8
+# b = 3
+# print (a ** b)
+# print (a // b)
+# print (a % b)
+
+
